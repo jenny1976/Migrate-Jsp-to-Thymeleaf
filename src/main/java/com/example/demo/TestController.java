1b9fb9c23a105dfcf20e274308f18d09425857ce
Jenny S <jennykroete@gmx.de>
2018-04-01 11:06:47 +0200

    cleanup

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,10 +1,7 @@
 package com.example.demo;
 
 import com.example.demo.model.Stock;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,28 +14,14 @@ public class TestController {
     @GetMapping(path = {"/thymeleaf"})
     public ModelAndView showPage() {
 
-        ModelAndView model = new ModelAndView("stocks");
-        model.addObject("items", Stock.dummyItems());
-        return model;
-
-//        return getDefaultView(Optional.of("thymeleaf"), "/stocks");
+        return new ModelAndView("stocks", "items", Stock.dummyItems());
     }
 
     @GetMapping(path = {"/jsp"})
+
     public ModelAndView showJspPage() {
 
-        return getDefaultView(Optional.empty(), "/welcome");
+        return new ModelAndView("jsp/welcome", "items", Stock.dummyItems());
     }
 
-    private ModelAndView getDefaultView(Optional<String> viewResolver, String viewName) {
-        ModelAndView model = new ModelAndView(createView(viewResolver, viewName));
-        model.addObject("items", Stock.dummyItems());
-        return model;
-    }
-
-    private String createView(Optional<String> viewResolver, String viewName) {
-        String result = viewResolver.isPresent() ? viewResolver.get() : "jsp";
-        result += viewName;
-        return result;
-    }
 }
--- src/test/java/com/example/demo/TestControllerTest.java
+++ src/test/java/com/example/demo/TestControllerTest.java
@@ -1,9 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.example.demo;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,11 +15,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -47,7 +43,8 @@ public class TestControllerTest {
         result
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(model().attributeExists("items"));
+                .andExpect(model().attributeExists("items"))
+                .andExpect(view().name("jsp/welcome"));
     }
 
     @Test
@@ -57,14 +54,7 @@ public class TestControllerTest {
         result
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(model().attributeExists("items"));
-    }
-
-    private void assertView(ResultActions result, String viewResolver) throws Exception {
-        result
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(model().attribute("name", "Spring Squad"))
-                .andExpect(view().name(viewResolver + "/home"));
+                .andExpect(model().attributeExists("items"))
+                .andExpect(view().name("stocks"));
     }
 }
